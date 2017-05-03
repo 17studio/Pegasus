d8273e9f2e68f300e9cd7e81999795be551e8ee7
17studio <17studio@gmail.com>
2017-05-04 00:17:33 +0800

    change clang-format ColumnLimit:120

--- deps/api/ctp/ctp_future_api/include/capi.h
+++ deps/api/ctp/ctp_future_api/include/capi.h
@@ -26,102 +26,67 @@ typedef struct ctp_md_api_t ctp_md_api_t;
 typedef void (*ctp_md_front_connected_cb)();
 typedef void (*ctp_md_front_disconnected_cb)(int reason);
 typedef void (*ctp_md_heart_beat_warning_cb)(int timelapse);
-typedef void (*ctp_md_user_login_rsp_cb)(
-    CThostFtdcRspUserLoginField* user_login_field,
-    CThostFtdcRspInfoField* info,
-    int request_id,
-    bool is_last);
-typedef void (*ctp_md_user_logout_rsp_cb)(
-    CThostFtdcUserLogoutField* user_logout_field,
-    CThostFtdcRspInfoField* info,
-    int request_id,
-    bool is_last);
-typedef void (*ctp_md_error_rsp_cb)(CThostFtdcRspInfoField* info,
-                                    int request_id,
-                                    bool is_last);
-typedef void (*ctp_md_sub_market_data_rsp_cb)(
-    CThostFtdcSpecificInstrumentField* specific_instrument,
-    CThostFtdcRspInfoField* info,
-    int request_id,
-    bool is_last);
-typedef void (*ctp_md_unsub_market_data_rsp_cb)(
-    CThostFtdcSpecificInstrumentField* specific_instrument,
-    CThostFtdcRspInfoField* info,
-    int request_id,
-    bool is_last);
-typedef void (*ctp_md_sub_quote_rsp_cb)(
-    CThostFtdcSpecificInstrumentField* specific_instrument,
-    CThostFtdcRspInfoField* info,
-    int request_id,
-    bool is_last);
-typedef void (*ctp_md_unsub_quote_rsp_cb)(
-    CThostFtdcSpecificInstrumentField* specific_instrument,
-    CThostFtdcRspInfoField* info,
-    int request_id,
-    bool is_last);
-typedef void (*ctp_md_depth_market_data_cb)(
-    CThostFtdcDepthMarketDataField* depth_market_data);
+typedef void (*ctp_md_user_login_rsp_cb)(CThostFtdcRspUserLoginField* user_login_field,
+                                         CThostFtdcRspInfoField* info,
+                                         int request_id,
+                                         bool is_last);
+typedef void (*ctp_md_user_logout_rsp_cb)(CThostFtdcUserLogoutField* user_logout_field,
+                                          CThostFtdcRspInfoField* info,
+                                          int request_id,
+                                          bool is_last);
+typedef void (*ctp_md_error_rsp_cb)(CThostFtdcRspInfoField* info, int request_id, bool is_last);
+typedef void (*ctp_md_sub_market_data_rsp_cb)(CThostFtdcSpecificInstrumentField* specific_instrument,
+                                              CThostFtdcRspInfoField* info,
+                                              int request_id,
+                                              bool is_last);
+typedef void (*ctp_md_unsub_market_data_rsp_cb)(CThostFtdcSpecificInstrumentField* specific_instrument,
+                                                CThostFtdcRspInfoField* info,
+                                                int request_id,
+                                                bool is_last);
+typedef void (*ctp_md_sub_quote_rsp_cb)(CThostFtdcSpecificInstrumentField* specific_instrument,
+                                        CThostFtdcRspInfoField* info,
+                                        int request_id,
+                                        bool is_last);
+typedef void (*ctp_md_unsub_quote_rsp_cb)(CThostFtdcSpecificInstrumentField* specific_instrument,
+                                          CThostFtdcRspInfoField* info,
+                                          int request_id,
+                                          bool is_last);
+typedef void (*ctp_md_depth_market_data_cb)(CThostFtdcDepthMarketDataField* depth_market_data);
 typedef void (*ctp_md_quote_cb)(CThostFtdcForQuoteRspField* quote);
 
 // functions
 
 ctp_md_spi_t* ctp_mdspi_create();
 void ctp_mdspi_free(ctp_md_spi_t* spi);
-void ctp_mdspi_set_front_connected_cb(ctp_md_spi_t* spi,
-                                      ctp_md_front_connected_cb cb);
-void ctp_mdspi_set_front_disconnected_cb(ctp_md_spi_t* spi,
-                                         ctp_md_front_disconnected_cb cb);
-void ctp_mdspi_set_heartbeat_warning_cb(ctp_md_spi_t* spi,
-                                        ctp_md_heart_beat_warning_cb cb);
-void ctp_mdspi_set_user_login_cb(ctp_md_spi_t* spi,
-                                 ctp_md_user_login_rsp_cb cb);
-void ctp_mdspi_set_user_logout_cb(ctp_md_spi_t* spi,
-                                  ctp_md_user_logout_rsp_cb cb);
+void ctp_mdspi_set_front_connected_cb(ctp_md_spi_t* spi, ctp_md_front_connected_cb cb);
+void ctp_mdspi_set_front_disconnected_cb(ctp_md_spi_t* spi, ctp_md_front_disconnected_cb cb);
+void ctp_mdspi_set_heartbeat_warning_cb(ctp_md_spi_t* spi, ctp_md_heart_beat_warning_cb cb);
+void ctp_mdspi_set_user_login_cb(ctp_md_spi_t* spi, ctp_md_user_login_rsp_cb cb);
+void ctp_mdspi_set_user_logout_cb(ctp_md_spi_t* spi, ctp_md_user_logout_rsp_cb cb);
 void ctp_mdspi_set_error_cb(ctp_md_spi_t* spi, ctp_md_error_rsp_cb cb);
-void ctp_mdspi_set_sub_market_data_rsp_cb(ctp_md_spi_t* spi,
-                                          ctp_md_sub_market_data_rsp_cb cb);
-void ctp_mdspi_set_unsub_market_data_rsp_cb(ctp_md_spi_t* spi,
-                                            ctp_md_unsub_market_data_rsp_cb cb);
-void ctp_mdspi_set_sub_quote_rsp_cb(ctp_md_spi_t* spi,
-                                    ctp_md_unsub_quote_rsp_cb cb);
-void ctp_mdspi_set_unsub_quote_rsp_cb(ctp_md_spi_t* spi,
-                                      ctp_md_unsub_quote_rsp_cb cb);
-void ctp_mdspi_set_depth_market_data_cb(ctp_md_spi_t* spi,
-                                        ctp_md_depth_market_data_cb cb);
+void ctp_mdspi_set_sub_market_data_rsp_cb(ctp_md_spi_t* spi, ctp_md_sub_market_data_rsp_cb cb);
+void ctp_mdspi_set_unsub_market_data_rsp_cb(ctp_md_spi_t* spi, ctp_md_unsub_market_data_rsp_cb cb);
+void ctp_mdspi_set_sub_quote_rsp_cb(ctp_md_spi_t* spi, ctp_md_unsub_quote_rsp_cb cb);
+void ctp_mdspi_set_unsub_quote_rsp_cb(ctp_md_spi_t* spi, ctp_md_unsub_quote_rsp_cb cb);
+void ctp_mdspi_set_depth_market_data_cb(ctp_md_spi_t* spi, ctp_md_depth_market_data_cb cb);
 void ctp_mdspi_set_quote_cb(ctp_md_spi_t* spi, ctp_md_quote_cb cb);
 
-ctp_md_api_t* ctp_mdapi_create(const char* flowpath,
-                               bool useudp,
-                               bool multicast);
+ctp_md_api_t* ctp_mdapi_create(const char* flowpath, bool useudp, bool multicast);
 void ctp_mdapi_free(ctp_md_api_t* api);
 const char* ctp_mdapi_get_api_version(ctp_md_api_t* api);
 void ctp_mdapi_init(ctp_md_api_t* api);
 void ctp_mdapi_join(ctp_md_api_t* api);
 const char* ctp_mdapi_get_trading_day(ctp_md_api_t* api);
 void ctp_mdapi_register_front(ctp_md_api_t* api, char* front_address);
-void ctp_mdapi_register_nameserver(ctp_md_api_t* api,
-                                   char* nameserver_address);
-void ctp_mdapi_register_fens_userinfo(ctp_md_api_t* api,
-                                      CThostFtdcFensUserInfoField* userinfo);
+void ctp_mdapi_register_nameserver(ctp_md_api_t* api, char* nameserver_address);
+void ctp_mdapi_register_fens_userinfo(ctp_md_api_t* api, CThostFtdcFensUserInfoField* userinfo);
 void ctp_mdapi_register_spi(ctp_md_api_t* api, ctp_md_spi_t* spi);
-void ctp_mdapi_sub_market_data(ctp_md_api_t* api,
-                               char* instruments[],
-                               int count);
-void ctp_mdapi_unsub_market_data(ctp_md_api_t* api,
-                                 char* instruments[],
-                                 int count);
-void ctp_mdapi_sub_quote(ctp_md_api_t* api,
-                         char* instruments[],
-                         int count);
-void ctp_mdapi_unsub_quote(ctp_md_api_t* api,
-                           char* instruments[],
-                           int count);
-void ctp_mdapi_user_login(ctp_md_api_t* api,
-                          CThostFtdcReqUserLoginField* userlogin,
-                          int request_id);
-void ctp_mdapi_user_logout(ctp_md_api_t* api,
-                           CThostFtdcUserLogoutField* userlogout,
-                           int request_id);
+void ctp_mdapi_sub_market_data(ctp_md_api_t* api, char* instruments[], int count);
+void ctp_mdapi_unsub_market_data(ctp_md_api_t* api, char* instruments[], int count);
+void ctp_mdapi_sub_quote(ctp_md_api_t* api, char* instruments[], int count);
+void ctp_mdapi_unsub_quote(ctp_md_api_t* api, char* instruments[], int count);
+void ctp_mdapi_user_login(ctp_md_api_t* api, CThostFtdcReqUserLoginField* userlogin, int request_id);
+void ctp_mdapi_user_logout(ctp_md_api_t* api, CThostFtdcUserLogoutField* userlogout, int request_id);
 
 #ifdef __cplusplus
 } /* end extern "C" */
--- deps/api/ctp/ctp_future_api/src/capi.cc
+++ deps/api/ctp/ctp_future_api/src/capi.cc
@@ -36,15 +36,11 @@ class CThostFtdcMdSpiImpl : public CThostFtdcMdSpi {
   ///        0x2001 接收心跳超时
   ///        0x2002 发送心跳失败
   ///        0x2003 收到错误报文
-  virtual void OnFrontDisconnected(int nReason) override {
-    callback_.disconnected(nReason);
-  };
+  virtual void OnFrontDisconnected(int nReason) override { callback_.disconnected(nReason); };
 
   ///心跳超时警告。当长时间未收到报文时，该方法被调用。
   ///@param nTimeLapse 距离上次接收报文的时间
-  virtual void OnHeartBeatWarning(int nTimeLapse) override {
-    callback_.heat_beat_warning(nTimeLapse);
-  };
+  virtual void OnHeartBeatWarning(int nTimeLapse) override { callback_.heat_beat_warning(nTimeLapse); };
 
   ///登录请求响应
   virtual void OnRspUserLogin(CThostFtdcRspUserLoginField* pRspUserLogin,
@@ -63,90 +59,62 @@ class CThostFtdcMdSpiImpl : public CThostFtdcMdSpi {
   };
 
   ///错误应答
-  virtual void OnRspError(CThostFtdcRspInfoField* pRspInfo,
-                          int nRequestID,
-                          bool bIsLast) override {
+  virtual void OnRspError(CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast) override {
     callback_.error(pRspInfo, nRequestID, bIsLast);
   };
 
   ///订阅行情应答
-  virtual void OnRspSubMarketData(
-      CThostFtdcSpecificInstrumentField* pSpecificInstrument,
-      CThostFtdcRspInfoField* pRspInfo,
-      int nRequestID,
-      bool bIsLast) override {
+  virtual void OnRspSubMarketData(CThostFtdcSpecificInstrumentField* pSpecificInstrument,
+                                  CThostFtdcRspInfoField* pRspInfo,
+                                  int nRequestID,
+                                  bool bIsLast) override {
     callback_.sub_market(pSpecificInstrument, pRspInfo, nRequestID, bIsLast);
   };
 
   ///取消订阅行情应答
-  virtual void OnRspUnSubMarketData(
-      CThostFtdcSpecificInstrumentField* pSpecificInstrument,
-      CThostFtdcRspInfoField* pRspInfo,
-      int nRequestID,
-      bool bIsLast) override {
+  virtual void OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField* pSpecificInstrument,
+                                    CThostFtdcRspInfoField* pRspInfo,
+                                    int nRequestID,
+                                    bool bIsLast) override {
     callback_.unsub_market(pSpecificInstrument, pRspInfo, nRequestID, bIsLast);
   };
 
   ///订阅询价应答
-  virtual void OnRspSubForQuoteRsp(
-      CThostFtdcSpecificInstrumentField* pSpecificInstrument,
-      CThostFtdcRspInfoField* pRspInfo,
-      int nRequestID,
-      bool bIsLast) override {
+  virtual void OnRspSubForQuoteRsp(CThostFtdcSpecificInstrumentField* pSpecificInstrument,
+                                   CThostFtdcRspInfoField* pRspInfo,
+                                   int nRequestID,
+                                   bool bIsLast) override {
     callback_.sub_quote(pSpecificInstrument, pRspInfo, nRequestID, bIsLast);
   };
 
   ///取消订阅询价应答
-  virtual void OnRspUnSubForQuoteRsp(
-      CThostFtdcSpecificInstrumentField* pSpecificInstrument,
-      CThostFtdcRspInfoField* pRspInfo,
-      int nRequestID,
-      bool bIsLast) override {
+  virtual void OnRspUnSubForQuoteRsp(CThostFtdcSpecificInstrumentField* pSpecificInstrument,
+                                     CThostFtdcRspInfoField* pRspInfo,
+                                     int nRequestID,
+                                     bool bIsLast) override {
     callback_.unsub_quote(pSpecificInstrument, pRspInfo, nRequestID, bIsLast);
   };
 
   ///深度行情通知
-  virtual void OnRtnDepthMarketData(
-      CThostFtdcDepthMarketDataField* pDepthMarketData) override {
+  virtual void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField* pDepthMarketData) override {
     callback_.market_data(pDepthMarketData);
   };
 
   ///询价通知
-  virtual void OnRtnForQuoteRsp(
-      CThostFtdcForQuoteRspField* pForQuoteRsp) override {
-    callback_.quote(pForQuoteRsp);
-  };
+  virtual void OnRtnForQuoteRsp(CThostFtdcForQuoteRspField* pForQuoteRsp) override { callback_.quote(pForQuoteRsp); };
 
   ///完成各项回掉的设置
-  void SetOnFrontConnected(ctp_md_front_connected_cb cb) {
-    callback_.connected = cb;
-  }
-  void SetOnFrontDisconnected(ctp_md_front_disconnected_cb cb) {
-    callback_.disconnected = cb;
-  }
-  void SetOnHeartBeatWarning(ctp_md_heart_beat_warning_cb cb) {
-    callback_.heat_beat_warning = cb;
-  }
+  void SetOnFrontConnected(ctp_md_front_connected_cb cb) { callback_.connected = cb; }
+  void SetOnFrontDisconnected(ctp_md_front_disconnected_cb cb) { callback_.disconnected = cb; }
+  void SetOnHeartBeatWarning(ctp_md_heart_beat_warning_cb cb) { callback_.heat_beat_warning = cb; }
   void SetOnRspUserLogin(ctp_md_user_login_rsp_cb cb) { callback_.login = cb; }
-  void SetOnRspUserLogout(ctp_md_user_logout_rsp_cb cb) {
-    callback_.logout = cb;
-  }
+  void SetOnRspUserLogout(ctp_md_user_logout_rsp_cb cb) { callback_.logout = cb; }
   void SetOnRspError(ctp_md_error_rsp_cb cb) { callback_.error = cb; }
-  void SetOnRspSubMarketData(ctp_md_sub_market_data_rsp_cb cb) {
-    callback_.sub_market = cb;
-  }
-  void SetOnRspUnSubMarketData(ctp_md_unsub_market_data_rsp_cb cb) {
-    callback_.unsub_market = cb;
-  }
-  void SetOnRspSubForQuoteRsp(ctp_md_sub_quote_rsp_cb cb) {
-    callback_.sub_quote = cb;
-  }
-  void SetOnRspUnSubForQuoteRsp(ctp_md_unsub_quote_rsp_cb cb) {
-    callback_.unsub_quote = cb;
-  }
-  void SetOnRtnDepthMarketData(ctp_md_depth_market_data_cb cb) {
-    callback_.market_data = cb;
-  }
+  void SetOnRspSubMarketData(ctp_md_sub_market_data_rsp_cb cb) { callback_.sub_market = cb; }
+  void SetOnRspUnSubMarketData(ctp_md_unsub_market_data_rsp_cb cb) { callback_.unsub_market = cb; }
+  void SetOnRspSubForQuoteRsp(ctp_md_sub_quote_rsp_cb cb) { callback_.sub_quote = cb; }
+  void SetOnRspUnSubForQuoteRsp(ctp_md_unsub_quote_rsp_cb cb) { callback_.unsub_quote = cb; }
+  void SetOnRtnDepthMarketData(ctp_md_depth_market_data_cb cb) { callback_.market_data = cb; }
   void SetOnRtnForQuoteRsp(ctp_md_quote_cb cb) { callback_.quote = cb; }
 
  private:
@@ -181,57 +149,44 @@ ctp_md_spi_t* ctp_mdspi_create() {
 void ctp_mdspi_free(ctp_md_spi_t* spi) {
   delete spi;
 }
-void ctp_mdspi_set_front_connected_cb(ctp_md_spi_t* spi,
-                                      ctp_md_front_connected_cb cb) {
+void ctp_mdspi_set_front_connected_cb(ctp_md_spi_t* spi, ctp_md_front_connected_cb cb) {
   spi->impl.SetOnFrontConnected(cb);
 }
-void ctp_mdspi_set_front_disconnected_cb(ctp_md_spi_t* spi,
-                                         ctp_md_front_disconnected_cb cb) {
+void ctp_mdspi_set_front_disconnected_cb(ctp_md_spi_t* spi, ctp_md_front_disconnected_cb cb) {
   spi->impl.SetOnFrontDisconnected(cb);
 }
-void ctp_mdspi_set_heartbeat_warning_cb(ctp_md_spi_t* spi,
-                                        ctp_md_heart_beat_warning_cb cb) {
+void ctp_mdspi_set_heartbeat_warning_cb(ctp_md_spi_t* spi, ctp_md_heart_beat_warning_cb cb) {
   spi->impl.SetOnHeartBeatWarning(cb);
 }
-void ctp_mdspi_set_user_login_cb(ctp_md_spi_t* spi,
-                                 ctp_md_user_login_rsp_cb cb) {
+void ctp_mdspi_set_user_login_cb(ctp_md_spi_t* spi, ctp_md_user_login_rsp_cb cb) {
   spi->impl.SetOnRspUserLogin(cb);
 }
-void ctp_mdspi_set_user_logout_cb(ctp_md_spi_t* spi,
-                                  ctp_md_user_logout_rsp_cb cb) {
+void ctp_mdspi_set_user_logout_cb(ctp_md_spi_t* spi, ctp_md_user_logout_rsp_cb cb) {
   spi->impl.SetOnRspUserLogout(cb);
 }
 void ctp_mdspi_set_error_cb(ctp_md_spi_t* spi, ctp_md_error_rsp_cb cb) {
   spi->impl.SetOnRspError(cb);
 }
-void ctp_mdspi_set_sub_market_data_rsp_cb(ctp_md_spi_t* spi,
-                                          ctp_md_sub_market_data_rsp_cb cb) {
+void ctp_mdspi_set_sub_market_data_rsp_cb(ctp_md_spi_t* spi, ctp_md_sub_market_data_rsp_cb cb) {
   spi->impl.SetOnRspSubMarketData(cb);
 }
-void ctp_mdspi_set_unsub_market_data_rsp_cb(
-    ctp_md_spi_t* spi,
-    ctp_md_unsub_market_data_rsp_cb cb) {
+void ctp_mdspi_set_unsub_market_data_rsp_cb(ctp_md_spi_t* spi, ctp_md_unsub_market_data_rsp_cb cb) {
   spi->impl.SetOnRspUnSubMarketData(cb);
 }
-void ctp_mdspi_set_sub_quote_rsp_cb(ctp_md_spi_t* spi,
-                                    ctp_md_unsub_quote_rsp_cb cb) {
+void ctp_mdspi_set_sub_quote_rsp_cb(ctp_md_spi_t* spi, ctp_md_unsub_quote_rsp_cb cb) {
   spi->impl.SetOnRspSubForQuoteRsp(cb);
 }
-void ctp_mdspi_set_unsub_quote_rsp_cb(ctp_md_spi_t* spi,
-                                      ctp_md_unsub_quote_rsp_cb cb) {
+void ctp_mdspi_set_unsub_quote_rsp_cb(ctp_md_spi_t* spi, ctp_md_unsub_quote_rsp_cb cb) {
   spi->impl.SetOnRspUnSubForQuoteRsp(cb);
 }
-void ctp_mdspi_set_depth_market_data_cb(ctp_md_spi_t* spi,
-                                        ctp_md_depth_market_data_cb cb) {
+void ctp_mdspi_set_depth_market_data_cb(ctp_md_spi_t* spi, ctp_md_depth_market_data_cb cb) {
   spi->impl.SetOnRtnDepthMarketData(cb);
 }
 void ctp_mdspi_set_quote_cb(ctp_md_spi_t* spi, ctp_md_quote_cb cb) {
   spi->impl.SetOnRtnForQuoteRsp(cb);
 }
 
-ctp_md_api_t* ctp_mdapi_create(const char* flowpath,
-                               bool useudp,
-                               bool multicast) {
+ctp_md_api_t* ctp_mdapi_create(const char* flowpath, bool useudp, bool multicast) {
   auto api = new ctp_md_api_t;
   api->mdapi = CThostFtdcMdApi::CreateFtdcMdApi(flowpath, useudp, multicast);
   return api;
@@ -255,45 +210,31 @@ const char* ctp_mdapi_get_trading_day(ctp_md_api_t* api) {
 void ctp_mdapi_register_front(ctp_md_api_t* api, char* front_address) {
   api->mdapi->RegisterFront(front_address);
 }
-void ctp_mdapi_register_nameserver(ctp_md_api_t* api,
-                                   char* nameserver_address) {
+void ctp_mdapi_register_nameserver(ctp_md_api_t* api, char* nameserver_address) {
   api->mdapi->RegisterNameServer(nameserver_address);
 }
-void ctp_mdapi_register_fens_userinfo(ctp_md_api_t* api,
-                                      CThostFtdcFensUserInfoField* userinfo) {
+void ctp_mdapi_register_fens_userinfo(ctp_md_api_t* api, CThostFtdcFensUserInfoField* userinfo) {
   api->mdapi->RegisterFensUserInfo(userinfo);
 }
 void ctp_mdapi_register_spi(ctp_md_api_t* api, ctp_md_spi_t* spi) {
   api->mdapi->RegisterSpi(&(spi->impl));
 }
-void ctp_mdapi_sub_market_data(ctp_md_api_t* api,
-                               char* instruments[],
-                               int count) {
+void ctp_mdapi_sub_market_data(ctp_md_api_t* api, char* instruments[], int count) {
   api->mdapi->SubscribeMarketData(instruments, count);
 }
-void ctp_mdapi_unsub_market_data(ctp_md_api_t* api,
-                                 char* instruments[],
-                                 int count) {
+void ctp_mdapi_unsub_market_data(ctp_md_api_t* api, char* instruments[], int count) {
   api->mdapi->UnSubscribeMarketData(instruments, count);
 }
-void ctp_mdapi_sub_quote(ctp_md_api_t* api,
-                         char* instruments[],
-                         int count) {
+void ctp_mdapi_sub_quote(ctp_md_api_t* api, char* instruments[], int count) {
   api->mdapi->SubscribeForQuoteRsp(instruments, count);
 }
-void ctp_mdapi_unsub_quote(ctp_md_api_t* api,
-                           char* instruments[],
-                           int count) {
+void ctp_mdapi_unsub_quote(ctp_md_api_t* api, char* instruments[], int count) {
   api->mdapi->UnSubscribeForQuoteRsp(instruments, count);
 }
-void ctp_mdapi_user_login(ctp_md_api_t* api,
-                          CThostFtdcReqUserLoginField* userlogin,
-                          int request_id) {
+void ctp_mdapi_user_login(ctp_md_api_t* api, CThostFtdcReqUserLoginField* userlogin, int request_id) {
   api->mdapi->ReqUserLogin(userlogin, request_id);
 }
-void ctp_mdapi_user_logout(ctp_md_api_t* api,
-                           CThostFtdcUserLogoutField* userlogout,
-                           int request_id) {
+void ctp_mdapi_user_logout(ctp_md_api_t* api, CThostFtdcUserLogoutField* userlogout, int request_id) {
   api->mdapi->ReqUserLogout(userlogout, request_id);
 }
 
